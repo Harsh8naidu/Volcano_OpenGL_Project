8e1d6f9b085af380a9810da1dc35248e9806f8b9
Harsh8naidu <harsh8naidu@gmail.com>
2024-11-13 23:17:28 +0000

    Add project files.

---
+++ Volcano_OpenGL_Project/Volcano_OpenGL_Project.cpp
@@ -0,0 +1,9 @@
+// Volcano_OpenGL_Project.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
