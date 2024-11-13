457c68c40eb6d6f9593c0bf459774129f18f95c2
Harsh8naidu <harsh8naidu@gmail.com>
2024-11-13 23:46:21 +0000

    Setup open gl window using sdl library

--- Volcano_OpenGL_Project/Volcano_OpenGL_Project.cpp
+++ Volcano_OpenGL_Project/Volcano_OpenGL_Project.cpp
@@ -1,9 +1,77 @@
 // Volcano_OpenGL_Project.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
-
+#include <Windows.h>
 #include <iostream>
+#include <SDL2/SDL.h>
+#include <GL/glew.h>
+#include <GL/GL.h>
+
+#undef main
+
+SDL_Window* window;
+SDL_GLContext context;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	if (SDL_Init(SDL_INIT_VIDEO) < 0)
+	{
+		std::cout << "SDL could not initialize! SDL_Error: " << SDL_GetError() << std::endl;
+		return 1;
+	}
+
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
+
+	window = SDL_CreateWindow("Volcano OpenGL Project", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_OPENGL);
+
+	if (window == NULL)
+	{
+		std::cout << "Window could not be created! SDL_Error: " << SDL_GetError() << std::endl;
+		return 1;
+	}
+
+	context = SDL_GL_CreateContext(window);
+
+	if (context == NULL)
+	{
+		SDL_DestroyWindow(window);
+		SDL_Quit();
+		std::cout << "OpenGL context could not be created! SDL_Error: " << SDL_GetError() << std::endl;
+		return 1;
+	}
+	
+	if (glewInit() != GLEW_OK)
+	{
+		SDL_GL_DeleteContext(context);
+		SDL_DestroyWindow(window);
+		SDL_Quit();
+		std::cout << "GLEW could not initialize!" << std::endl;
+		return 1;
+	}
+
+	bool stopped = false;
+
+	while (!stopped)
+	{
+		SDL_Event event;
+		while (SDL_PollEvent(&event))
+		{
+			if (event.type == SDL_QUIT)
+			{
+				stopped = true;
+			}
+		}
+
+		// render the scene
+		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		SDL_GL_SwapWindow(window);
+	}
+
+	SDL_GL_DeleteContext(context);
+	SDL_DestroyWindow(window);
+	SDL_Quit();
+
+	return 0;
 }
